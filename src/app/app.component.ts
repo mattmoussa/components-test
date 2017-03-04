cd5d2c383e652bd5731bcc9a050e656ae214666e
mattmoussa <super1upman@gmail.com>
2017-03-04 08:40:50 -0500

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  headerContent:String;
+  pictureContent:String;
+  inputContent:String;
+  buttonContent:String;
+  footerContent:String;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,24 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ButtonComponent } from './button/button.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { InputComponent } from './input/input.component';
+import { PictureComponent } from './picture/picture.component';
+import { DisclaimerComponent } from './disclaimer/disclaimer.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonComponent,
+    HeaderComponent,
+    FooterComponent,
+    InputComponent,
+    PictureComponent,
+    DisclaimerComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
